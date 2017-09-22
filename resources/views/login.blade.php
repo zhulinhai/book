7fb6bcd19541d5a991154522c254a8a4cf675882
zhulinhai <zhulh128@163.com>
2017-09-22 18:01:14 +0800

    增加weui

---
+++ resources/views/login.blade.php
@@ -0,0 +1,12 @@
+@extends('master')
+@section('title', '登录')
+
+@section('content')
+    <p>这是登录</p>
+@endsection
+
+@section('my-js')
+    <script type="text/javascript">
+        alert('登录');
+    </script>
+@endsection
---
+++ resources/views/master.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+//*http://www.maiziedu.com/course/649-9643/*/
+
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=0">
+    <title>@yield('title')</title>
+    <link rel="stylesheet" href="./css/weui.css">
+    <link rel="stylesheet" href="./css/book.css">
+</head>
+<body>
+@yield('content')
+</body>
+<script src="./js/jquery-1.10.1.min.js"></script>
+@yield('my-js')
+</html>
